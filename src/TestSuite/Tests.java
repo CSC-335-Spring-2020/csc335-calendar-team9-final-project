81248483a422798e1a59c208bc76f259d350cb52
Logan <minedragonzz@gmail.com>
2020-05-06 18:14:00 -0700

    83% test coverage.

--- src/TestSuite/Tests.java
+++ src/TestSuite/Tests.java
@@ -33,10 +33,9 @@ public class Tests {
 	void testConstructor() {
 		int currYear = 2020;
 		CalendarController controller =  new CalendarController(currYear);
+		assertEquals(2020, controller.getYear());
 		controller.save();
 		controller =  new CalendarController(currYear);
-		assertEquals(2020, controller.getYear());
-		
 	}
 	
 	/**
